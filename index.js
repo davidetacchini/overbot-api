b9b74421235282c654ca7fa3ae786f954ad8e97e
davide <davidetacchini11@gmail.com>
2020-11-18 19:20:28 +0100

    Use authenticateToken middleware globally

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import routes from './routes/index.js';
 import { notFound, errorHandler } from './middlewares/errors.js';
+import { authenticateToken } from './middlewares/auth.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,9 +11,10 @@ const port = process.env.PORT || 5000;
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
+app.use(authenticateToken);
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Go to /api' });
+  res.status(200).json({ message: 'Go to /api' });
 });
 
 app.use('/api', routes);
