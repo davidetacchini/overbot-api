02f7b3ae98c78f524311e90b230c1eb4b52b7451
davide <davidetacchini11@gmail.com>
2020-11-18 19:21:27 +0100

    One line status and json response

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -6,8 +6,7 @@ export const notFound = (req, res, next) => {
 
 export const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
-  res.status(statusCode);
-  res.json({
+  res.status(statusCode).json({
     message: err.message,
     stack: err.stack,
   });
