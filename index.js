53793c665273cccfe9c51382d93297cc34576ca1
davide <davidetacchini11@gmail.com>
2020-11-18 13:39:22 +0100

    Using cors; formatted

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 import routes from './routes/index.js';
-import { notFound, errorHandler } from './middlewares/errors.js'
+import { notFound, errorHandler } from './middlewares/errors.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -18,7 +21,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
-})
+    console.log(`Server is running on port: ${port}`);
+});
 
 export default app
