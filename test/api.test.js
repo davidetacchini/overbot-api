2063ffda623360bdaf3501c4a42a0e646cca3ca7
davide <davidetacchini11@gmail.com>
2020-11-18 22:48:43 +0100

    Add test for all routes

--- test/api.test.js
+++ test/api.test.js
@@ -17,3 +17,157 @@ describe('GET /api', () => {
       );
   });
 });
+
+describe('POST /api/statistics', () => {
+  it('should post bot, host and shards statistics', (done) => {
+    request(app)
+      .post('/api/statistics')
+      .set('Authorization', secretToken)
+      .send({
+        bot: { Ping: 100 },
+        host: { Python: '3.x' },
+        shards: { 1: 100 },
+      })
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .end(function (err, res) {
+        if (err) done(err);
+        done();
+      });
+  });
+});
+
+describe('GET /api/statistics', () => {
+  it('should respond with bot, host and shards statistics', (done) => {
+    request(app)
+      .get('/api/statistics')
+      .set('Authorization', secretToken)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        {
+          bot: { Ping: 100 },
+          host: { Python: '3.x' },
+          shards: { 1: 100 },
+        },
+        done
+      );
+  });
+});
+
+describe('POST /api/commands', () => {
+  it('should post a list of commands', (done) => {
+    request(app)
+      .post('/api/commands')
+      .set('Authorization', secretToken)
+      .send([
+        {
+          name: 'Rank',
+          description: 'Returns player ranks',
+        },
+      ])
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .end(function (err, res) {
+        if (err) done(err);
+        done();
+      });
+  });
+});
+
+describe('GET /api/commands', () => {
+  it('should respond with a list of commands', (done) => {
+    request(app)
+      .get('/api/commands')
+      .set('Authorization', secretToken)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        [
+          {
+            name: 'Rank',
+            description: 'Returns player ranks',
+          },
+        ],
+        done
+      );
+  });
+});
+
+describe('GET /api/commands/rank', () => {
+  it('should respond with a command object', (done) => {
+    request(app)
+      .get('/api/commands/rank')
+      .set('Authorization', secretToken)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        {
+          name: 'Rank',
+          description: 'Returns player ranks',
+        },
+        done
+      );
+  });
+});
+
+describe('POST /api/servers', () => {
+  it('should post a list of servers', (done) => {
+    request(app)
+      .post('/api/servers')
+      .set('Authorization', secretToken)
+      .send([
+        {
+          id: 123456789,
+          name: 'Server name',
+        },
+      ])
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .end(function (err, res) {
+        if (err) done(err);
+        done();
+      });
+  });
+});
+
+describe('GET /api/servers', () => {
+  it('should respond with a list of servers', (done) => {
+    request(app)
+      .get('/api/servers')
+      .set('Authorization', secretToken)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        [
+          {
+            id: 123456789,
+            name: 'Server name',
+          },
+        ],
+        done
+      );
+  });
+});
+
+describe('GET /api/servers/123456789', () => {
+  it('should respond with a server object', (done) => {
+    request(app)
+      .get('/api/servers/123456789')
+      .set('Authorization', secretToken)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(
+        200,
+        {
+          id: 123456789,
+          name: 'Server name',
+        },
+        done
+      );
+  });
+});
